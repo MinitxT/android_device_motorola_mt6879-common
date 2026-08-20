469bcb411c6ab6169ca9a326baddbc963f395281
Mashopy <eliasgheeraert@gmail.com>
2026-08-20 21:23:30 +0200

    mt6879-common: Properly patch blobs related to Stagefright
    C2 service doesn't depend on libstagefright_foundation.so so there is no need to add
    libstagefright_foundation-v33.so dependency in it. It's the C2 vdec/venc and plugin libs that has references for it.
    
    And let's patch the sensor HAL too just in case.
    
    There is no need to patch the system libs that depends over it as they aren't built
    over Android 12 like the vendor libs are.
    
    Change-Id: I3d5dc605788c6bf6edb429bee7c6921c6db9f5ed

--- extract-files.py
+++ extract-files.py
@@ -77,6 +77,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libformatter.so', 'libformatter-mtk.so'),
     'vendor/bin/hw/mtkfusionrild': blob_fixup()
         .add_needed('libutils-v32.so'),
+    ('vendor/lib64/hw/sensors.mediatek.V2.0.so', 'vendor/lib64/libcodec2_mtk_c2store.so', 'vendor/lib64/libcodec2_mtk_vdec.so', 'vendor/lib64/libcodec2_mtk_venc.so', 'vendor/lib64/libcodec2_vpp_qt_plugin.so', 'vendor/lib64/libcodec2_vpp_rs_plugin.so'): blob_fixup()
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     ('vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so', 'vendor/bin/hw/android.hardware.gnss-service.mediatek'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
     'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
